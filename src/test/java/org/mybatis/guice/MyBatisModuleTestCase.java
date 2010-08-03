2fbd371b0deeae2d91dfb11e1f46772ace185258
Simone Tripodi <simone.tripodi@gmail.com>
2010-08-03 19:55:38 +0000

    fixed broken import

--- src/test/java/org/mybatis/guice/MyBatisModuleTestCase.java
+++ src/test/java/org/mybatis/guice/MyBatisModuleTestCase.java
@@ -26,7 +26,7 @@ import javax.sql.DataSource;
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.junit.Before;
 import org.junit.Test;
-import org.mybatis.guice.datasource.PooledDataSourceProvider;
+import org.mybatis.guice.datasource.builtin.PooledDataSourceProvider;
 
 import com.google.inject.Binder;
 import com.google.inject.Guice;
