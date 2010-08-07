166c98575a55ce195c4c88ee91132c3639a9dda0
Simone Tripodi <simone.tripodi@gmail.com>
2010-08-07 09:08:39 +0000

    added custom exception message implementation

--- src/main/java/org/mybatis/guice/transactional/TransactionalMethodInterceptor.java
+++ src/main/java/org/mybatis/guice/transactional/TransactionalMethodInterceptor.java
@@ -17,6 +17,8 @@ package org.mybatis.guice.transactional;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.text.MessageFormat;
+import java.util.Arrays;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
@@ -33,6 +35,10 @@ import com.google.inject.Inject;
  * @version $Id$
  */
 public final class TransactionalMethodInterceptor implements MethodInterceptor {
+
+    private static final Class<?>[] CAUSE_TYPES = new Class[]{ Throwable.class };
+
+    private static final Class<?>[] MESSAGE_CAUSE_TYPES = new Class[]{ String.class, Throwable.class };
 
     /**
      * This class logger.
@@ -114,19 +120,43 @@ public final class TransactionalMethodInterceptor implements MethodInterceptor {
             }
 
             // rethrow the exception as new exception
-            Constructor<? extends Throwable> constructor =
-                transactional.rethrowExceptionsAs().getConstructor(Throwable.class);
+            String errorMessage;
+            Object[] initargs;
+            Class<?>[] initargsType;
 
+            if (transactional.exceptionMessage().length() != 0) {
+                errorMessage = MessageFormat.format(transactional.exceptionMessage(), invocation.getArguments());
+                initargs = new Object[]{ errorMessage, t };
+                initargsType = MESSAGE_CAUSE_TYPES;
+            } else {
+                initargs = new Object[]{ t };
+                initargsType = CAUSE_TYPES;
+            }
+
+            Constructor<? extends Throwable> exceptionConstructor = getMatchingConstructor(transactional.rethrowExceptionsAs(), initargsType);
             Throwable rethrowEx = null;
-
-            try {
-                rethrowEx = constructor.newInstance(t);
-            } catch (Exception e) {
-                String errorMessage = "Impossible to re-throw '"
+            if (exceptionConstructor != null) {
+                try {
+                    rethrowEx = exceptionConstructor.newInstance(initargs);
+                } catch (Exception e) {
+                    errorMessage = "Impossible to re-throw '"
+                        + transactional.rethrowExceptionsAs().getName()
+                        + "', it needs the constructor with "
+                        + Arrays.toString(initargsType)
+                        + " argument(s).";
+                    this.log.error(errorMessage, e);
+                    rethrowEx = new RuntimeException(errorMessage, e);
+                }
+            } else {
+                errorMessage = "Impossible to re-throw '"
                     + transactional.rethrowExceptionsAs().getName()
-                    + "', it needs the constructor with <Throwable> argument.";
-                this.log.error(errorMessage, e);
-                rethrowEx = new RuntimeException(errorMessage, e);
+                    + "', it needs the constructor with "
+                    + Arrays.toString(CAUSE_TYPES)
+                    + " or "
+                    + Arrays.toString(MESSAGE_CAUSE_TYPES)
+                    + " arguments.";
+                this.log.error(errorMessage);
+                rethrowEx = new RuntimeException(errorMessage);
             }
 
             throw rethrowEx;
@@ -150,6 +180,21 @@ public final class TransactionalMethodInterceptor implements MethodInterceptor {
         }
 
         return object;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <E extends Throwable> Constructor<E> getMatchingConstructor(Class<E> type,
+            Class<?>[] argumentsType) {
+        Class<? super E> currentType = type;
+        while (Object.class != currentType) {
+            for (Constructor<?> constructor : currentType.getConstructors()) {
+                if (Arrays.equals(argumentsType, constructor.getParameterTypes())) {
+                    return (Constructor<E>) constructor;
+                }
+            }
+            currentType = currentType.getSuperclass();
+        }
+        return null;
     }
 
 }
