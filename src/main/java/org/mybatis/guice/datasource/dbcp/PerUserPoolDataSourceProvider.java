3075d6d7738f1bd706b0fe8587a24197a6cdfa31
Simone Tripodi <simone.tripodi@gmail.com>
2012-03-28 20:45:32 +0000

    removed unused `this` references

--- src/main/java/org/mybatis/guice/datasource/dbcp/PerUserPoolDataSourceProvider.java
+++ src/main/java/org/mybatis/guice/datasource/dbcp/PerUserPoolDataSourceProvider.java
@@ -36,143 +36,143 @@ public final class PerUserPoolDataSourceProvider implements Provider<DataSource>
 
     @com.google.inject.Inject(optional = true)
     public void setConnectionPoolDataSource(ConnectionPoolDataSource cpds) {
-        this.dataSource.setConnectionPoolDataSource(cpds);
+        dataSource.setConnectionPoolDataSource(cpds);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setDataSourceName(@Named("DBCP.name") String name) {
-        this.dataSource.setDataSourceName(name);
+        dataSource.setDataSourceName(name);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setDefaultAutoCommit(@Named("JDBC.autoCommit") boolean autoCommit) {
-        this.dataSource.setDefaultAutoCommit(autoCommit);
+        dataSource.setDefaultAutoCommit(autoCommit);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setDefaultReadOnly(@Named("DBCP.defaultReadOnly") boolean defaultReadOnly) {
-        this.dataSource.setDefaultReadOnly(defaultReadOnly);
+        dataSource.setDefaultReadOnly(defaultReadOnly);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setDefaultTransactionIsolation(@Named("DBCP.defaultTransactionIsolation") int defaultTransactionIsolation) {
-        this.dataSource.setDefaultTransactionIsolation(defaultTransactionIsolation);
+        dataSource.setDefaultTransactionIsolation(defaultTransactionIsolation);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setDescription(@Named("DBCP.description") String description) {
-        this.dataSource.setDescription(description);
+        dataSource.setDescription(description);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setJndiEnvironment(@Named("DBCP.jndi.key") String key, @Named("DBCP.jndi.value") String value) {
-        this.dataSource.setJndiEnvironment(key, value);
+        dataSource.setJndiEnvironment(key, value);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setLoginTimeout(@Named("JDBC.loginTimeout") int loginTimeout) {
-        this.dataSource.setLoginTimeout(loginTimeout);
+        dataSource.setLoginTimeout(loginTimeout);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setMinEvictableIdleTimeMillis(@Named("DBCP.minEvictableIdleTimeMillis") int minEvictableIdleTimeMillis) {
-        this.dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setNumTestsPerEvictionRun(@Named("DBCP.numTestsPerEvictionRun") int numTestsPerEvictionRun) {
-        this.dataSource.setNumTestsPerEvictionRun(numTestsPerEvictionRun);
+        dataSource.setNumTestsPerEvictionRun(numTestsPerEvictionRun);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setRollbackAfterValidation(@Named("DBCP.rollbackAfterValidation") boolean rollbackAfterValidation) {
-        this.dataSource.setRollbackAfterValidation(rollbackAfterValidation);
+        dataSource.setRollbackAfterValidation(rollbackAfterValidation);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setTestOnBorrow(@Named("DBCP.testOnBorrow") boolean testOnBorrow) {
-        this.dataSource.setTestOnBorrow(testOnBorrow);
+        dataSource.setTestOnBorrow(testOnBorrow);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setTestOnReturn(@Named("DBCP.testOnReturn") boolean testOnReturn) {
-        this.dataSource.setTestOnReturn(testOnReturn);
+        dataSource.setTestOnReturn(testOnReturn);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setTestWhileIdle(@Named("DBCP.testWhileIdle") boolean testWhileIdle) {
-        this.dataSource.setTestWhileIdle(testWhileIdle);
+        dataSource.setTestWhileIdle(testWhileIdle);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setTimeBetweenEvictionRunsMillis(@Named("DBCP.timeBetweenEvictionRunsMillis") int timeBetweenEvictionRunsMillis) {
-        this.dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setValidationQuery(@Named("DBCP.validationQuery") String validationQuery) {
-        this.dataSource.setValidationQuery(validationQuery);
+        dataSource.setValidationQuery(validationQuery);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setDefaultMaxActive(@Named("DBCP.maxActive") int maxActive) {
-        this.dataSource.setDefaultMaxActive(maxActive);
+        dataSource.setDefaultMaxActive(maxActive);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setDefaultMaxIdle(@Named("DBCP.maxIdle") int defaultMaxIdle) {
-        this.dataSource.setDefaultMaxIdle(defaultMaxIdle);
+        dataSource.setDefaultMaxIdle(defaultMaxIdle);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setDefaultMaxWait(@Named("DBCP.maxWait") int defaultMaxWait) {
-        this.dataSource.setDefaultMaxWait(defaultMaxWait);
+        dataSource.setDefaultMaxWait(defaultMaxWait);
     }
 
     @com.google.inject.Inject(optional = true)
     public void setPerUserDefaultAutoCommit(@PerUserDefaultAutoCommit Map<String, Boolean> perUserDefaultAutoCommit) {
         for (Entry<String, Boolean> entry : perUserDefaultAutoCommit.entrySet()) {
-            this.dataSource.setPerUserDefaultAutoCommit(entry.getKey(), entry.getValue());
+            dataSource.setPerUserDefaultAutoCommit(entry.getKey(), entry.getValue());
         }
     }
 
     @com.google.inject.Inject(optional = true)
     public void setPerUserDefaultReadOnly(@PerUserDefaultReadOnly Map<String, Boolean> perUserDefaultReadOnly) {
         for (Entry<String, Boolean> entry : perUserDefaultReadOnly.entrySet()) {
-            this.dataSource.setPerUserDefaultReadOnly(entry.getKey(), entry.getValue());
+            dataSource.setPerUserDefaultReadOnly(entry.getKey(), entry.getValue());
         }
     }
 
     @com.google.inject.Inject(optional = true)
     public void setPerUserDefaultTransactionIsolation(@PerUserDefaultTransactionIsolation Map<String, Integer> perUserDefaultTransactionIsolation) {
         for (Entry<String, Integer> entry : perUserDefaultTransactionIsolation.entrySet()) {
-            this.dataSource.setPerUserDefaultTransactionIsolation(entry.getKey(), entry.getValue());
+            dataSource.setPerUserDefaultTransactionIsolation(entry.getKey(), entry.getValue());
         }
     }
 
     @com.google.inject.Inject(optional = true)
     public void setPerUserMaxActive(@PerUserMaxActive Map<String, Integer> perUserMaxActive) {
         for (Entry<String, Integer> entry : perUserMaxActive.entrySet()) {
-            this.dataSource.setPerUserMaxActive(entry.getKey(), entry.getValue());
+            dataSource.setPerUserMaxActive(entry.getKey(), entry.getValue());
         }
     }
 
     @com.google.inject.Inject(optional = true)
     public void setPerUserMaxIdle(@PerUserMaxIdle Map<String, Integer> perUserMaxIdle) {
         for (Entry<String, Integer> entry : perUserMaxIdle.entrySet()) {
-            this.dataSource.setPerUserMaxIdle(entry.getKey(), entry.getValue());
+            dataSource.setPerUserMaxIdle(entry.getKey(), entry.getValue());
         }
     }
 
     @com.google.inject.Inject(optional = true)
     public void setPerUserMaxWait(@PerUserMaxWait Map<String, Integer> perUserMaxWait) {
         for (Entry<String, Integer> entry : perUserMaxWait.entrySet()) {
-            this.dataSource.setPerUserMaxWait(entry.getKey(), entry.getValue());
+            dataSource.setPerUserMaxWait(entry.getKey(), entry.getValue());
         }
     }
 
     public DataSource get() {
-        return this.dataSource;
+        return dataSource;
     }
 
 }
